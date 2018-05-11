d988a3e4471c432723f3b3ef1a0c5f9469633ff0
Victor Ramirez <vgramirezc@unal.edu.co>
2018-05-11 16:59:07 -0500

    Change microsevices urls

--- src/grades/server.js
+++ src/grades/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.GRADES_URL || '0.0.0.0';
+export const url = process.env.GRADES_URL || '192.168.99.101';
 export const port = process.env.GRADES_PORT || '4002';
 export const entryPoint = process.env.GRADES_ENTRY || 'grades';
